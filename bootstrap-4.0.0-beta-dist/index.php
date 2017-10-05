f9841b3229a7e8e355ab2f829f1d75d85b0df354
Adorax <jeremy.maret@gmail.com>
2017-10-05 22:21:27 +0200

    php

---
+++ bootstrap-4.0.0-beta-dist/index.php
@@ -0,0 +1,9 @@
+<?php
+include('include/header.php') ?>
+
+<a class="beerRotate" href="page/carousel.html"><i class="fa fa-beer fa-spin fa-5x" aria-hidden="true"></i></a>
+
+
+
+<?php
+include('include/footer.php')?>
